1ff134c43fc916050cc34aaba7a1131f193f4a8e
xx <xx@qq.com>
2025-05-26 14:53:02 +0800

    update groovy impl

--- script-groovy/src/main/kotlin/ScriptGroovyMain.kt
+++ script-groovy/src/main/kotlin/ScriptGroovyMain.kt
@@ -1,3 +1,5 @@
+import groovy.lang.GroovyClassLoader
+import groovy.lang.GroovyObject
 import groovy.lang.GroovyShell
 import kotlin.time.measureTime
 
@@ -5,45 +7,34 @@ import kotlin.time.measureTime
  * @author Qnxy
  */
 
+private const val SCRIPT = """
+    class Fibonacci {
+        static def fib(n) {
+            if (n <= 1) return n
+            def a = 0, b = 1
+            (2..n).each {
+                (a, b) = [b, a + b]
+            }
+            return b
+        }
+    }
+"""
+
 fun main() {
 
-    val groovyShell = GroovyShell()
+    val groovyClassLoader = GroovyClassLoader()
+    val fibClass = groovyClassLoader.parseClass(SCRIPT)
+    val fibInstance = fibClass.getDeclaredConstructor().newInstance() as GroovyObject
 
     repeat(10000) {
-        groovyShell.evaluate(
-            """
-            static def fib(n) {
-                if (n <= 1) return n
-                def a = 0, b = 1
-                (2..n).each {
-                    (a, b) = [b, a + b]
-                }
-                return b
-            }
-            
-            fib(30)
-        """.trimIndent()
-        )
+        fibInstance.invokeMethod("fib", 30)
     }
 
     measureTime {
         repeat(10000) {
-            groovyShell.evaluate(
-                """
-            static def fib(n) {
-                if (n <= 1) return n
-                def a = 0, b = 1
-                (2..n).each {
-                    (a, b) = [b, a + b]
-                }
-                return b
-            }
-            
-            fib(30)
-            """.trimIndent()
-            )
+            fibInstance.invokeMethod("fib", 30)
         }
     }.also { println(it) }
 
 }
-// 13.337640055s
+// 20.999289ms
